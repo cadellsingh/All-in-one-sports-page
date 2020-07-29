6ed9e7c6c5ed382dc7ae599977d10a4a3dbb83ca
Cadell Singh <cadellsingh9@gmail.com>
2020-07-29 12:00:57 -0400

    is now only used for displaying selected sports

--- src/SidebarSports.js
+++ src/SidebarSports.js
@@ -1,42 +1,9 @@
-import React, { useState } from 'react'
-import Settings from './Settings'
+import React from 'react'
 
-const allSports = {
-  "NBA": true,
-  "NFL": true,
-  "MLB": true,
-  "Hockey": true,
-  "College Football": true,
-  "College Basketball": true,
-  "Soccer": true
-}
+const SidebarSports = (props) => {
+  const { sports } = props
 
-const SidebarSports = () => {
-  const [selectedSports, setSelectedSports] = useState(allSports)
-  const [displaySettings, setdisplaySettings] = useState(false)
-
-  const handleClick = () => {
-    setdisplaySettings(prevState => !prevState)
-  }
-
-  const toggleSelectedSport = (sport) => {
-    setSelectedSports(prevState => {
-      const updatedSports = { ...prevState }
-      updatedSports[sport] = !prevState[sport]
-      return updatedSports
-    })
-  }
-
-  // null here can be the main component with the data
-  // and prob re-name displayComponent
-  const displayComponent = displaySettings ?
-    <Settings
-      sports={selectedSports}
-      toggleSelectedSport={toggleSelectedSport}
-    />
-    : null
-
-  const entries = Object.entries(selectedSports)
+  const entries = Object.entries(sports)
   const displaySports = []
   for (const [sport, isSelected] of entries) {
     displaySports.push(isSelected && <li key={sport}>{sport}</li>)
@@ -45,15 +12,8 @@ const SidebarSports = () => {
   return (
     <div>
       {displaySports}
-      <h1 onClick={handleClick}>Settings</h1>
-      {displayComponent}
     </div>
   )
 }
 
 export default SidebarSports
-
-// possibly put useEffects in a handle button click
-// or
-// when sport is selected and goes to different component, that component takes sport as prop and fetches data
-// so the actual sport data will be in the other (child) component
