2edddc7f623ed548049566b627385f644baadbd3
Cadell Singh <cadellsingh9@gmail.com>
2020-07-29 11:59:59 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import SidebarSports from './SidebarSports';
+import Settings from './Settings';
 
 // possibly move state to this component
 // and re-structure components like:
@@ -21,11 +22,34 @@ const allSports = {
 const App = () => {
   const [selectedSports, setSelectedSports] = useState(allSports)
 
+  const toggleSelectedSport = (sport) => {
+    setSelectedSports(prevState => {
+      const updatedSports = { ...prevState }
+      updatedSports[sport] = !prevState[sport]
+      return updatedSports
+    })
+  }
+
   return (
     <div>
-      <SidebarSports />
+      <SidebarSports
+        sports={selectedSports}
+      />
+      <Settings
+        sports={selectedSports}
+        toggleSelectedSport={toggleSelectedSport}
+      />
+      {/* <MainComponentData/> */}
     </div>
   )
 }
 
 export default App;
+
+
+// settings and main component will be taking a prop -> whether or not to display component
+
+// possibly put useEffects in a handle button click
+// or
+// when sport is selected and goes to different component, that component takes sport as prop and fetches data
+// so the actual sport data will be in the other (child) component
