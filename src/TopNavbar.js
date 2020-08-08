c90aa95bafa04e41c2998be411558d334dc6beb0
Cadell Singh <cadellsingh9@gmail.com>
2020-08-08 14:05:55 -0400

    a

--- src/TopNavbar.js
+++ src/TopNavbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import SportButtons from "./SportButtons";
 
 const TopNavbar = () => {
   return (
     <Navbar variant="dark" bg="dark">
       <Navbar.Brand>All in one</Navbar.Brand>
+      {/* <Navbar.Brand>News</Navbar.Brand> */}
+      {/* <Navbar.Brand>Scores</Navbar.Brand> */}
     </Navbar>
   );
 };
