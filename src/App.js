ce20836b0b9d9ef9bffc905e698acf1806b28c9f
Cadell Singh <cadellsingh9@gmail.com>
2020-08-11 15:09:05 -0400

    moved api call to sportnews comp

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import SportsNews from "./SportsNews";
 import Sidebar from "./Sidebar";
 
-const newsUrls = [
-  "https://site.api.espn.com/apis/site/v2/sports/football/college-football/news",
-  "https://site.api.espn.com/apis/site/v2/sports/football/nfl/news",
-  "https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/news",
-  "https://site.api.espn.com/apis/site/v2/sports/hockey/nhl/news",
-  "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news",
-  "https://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/news",
-];
-
 const App = () => {
   const [sportToFetch, setSportToFetch] = useState("");
-  const [sportsNews, setSportsNews] = useState([]);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     for (const newsUrl of newsUrls) {
-  //       const res = await fetch(newsUrl);
-  //       res.json().then((res) => {
-  //         setSportsNews((state) => [...state, res]);
-  //       });
-  //     }
-  //   }
-
-  //   fetchData();
-  // }, []);
 
   const getSport = (sport) => {
     setSportToFetch(sport);
@@ -38,8 +15,8 @@ const App = () => {
   return (
     <Container fluid className="main-container">
       <Row>
-        <Sidebar />
-        <SportsNews sport={sportToFetch} sports={sportsNews} />
+        <Sidebar getSport={getSport}/>
+        <SportsNews sport={sportToFetch} />
       </Row>
     </Container>
   );
@@ -48,3 +25,5 @@ const App = () => {
 export default App;
 
 // may have to add onclick handlers for news & scores components
+
+// sidebar component -> contains Sport buttons
